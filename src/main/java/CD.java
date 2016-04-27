8919a608ab58f4b0aa4a0728da4f32bca23746fc
Fernanda Lowe and Inthrayuth Mahaphol <fernanda@>
2016-04-27 16:02:21 -0700

    pass all the back-end tests

--- src/main/java/CD.java
+++ src/main/java/CD.java
@@ -1,12 +1,44 @@
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+
 public class CD {
   private String mName;
-
+  private LocalDateTime mCreatedAt;
+  private static ArrayList<CD> instances = new ArrayList<CD>();
+  private int mId;
 
   public CD(String name){
     mName = name;
+    mCreatedAt = LocalDateTime.now();
+    instances.add(this);
+    mId = instances.size();
   }
 
   public String getName(){
     return mName;
   }
+
+  public LocalDateTime getCreatedAt() {
+    return mCreatedAt;
+  }
+
+  public static ArrayList<CD> all() {
+    return instances;
+  }
+
+  public static void clear() {
+    instances.clear();
+  }
+
+  public int getId() {
+    return mId;
+  }
+  public static CD find(int id) {
+    try {
+      return instances.get(id - 1);
+    } catch (IndexOutOfBoundsException exception) {
+      return null;
+    }
+  }
+
 }
--- src/test/java/CDTest.java
+++ src/test/java/CDTest.java
@@ -1,5 +1,6 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.time.LocalDateTime;
 
 public class CDTest {
 
@@ -14,5 +15,44 @@ public class CDTest {
     CD myCD = new CD("Songs of Innocence");
     assertEquals("Songs of Innocence", myCD.getName());
   }
-  
+
+  @Test
+  public void getCreatedAt_instantiatesWithCurrentTime_today() {
+    CD myCD = new CD("Songs of Innocence");
+    assertEquals(LocalDateTime.now().getDayOfWeek(), myCD.getCreatedAt().getDayOfWeek());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOf_CD_true() {
+    CD firstName = new CD("Songs of Innocence");
+    CD secondName = new CD("No Line on the Horizon");
+    assertTrue(CD.all().contains(firstName));
+    assertTrue(CD.all().contains(secondName));
+  }
+
+  @Test
+  public void clear_emptiesAllTasksFromArrayList_0() {
+    CD myCD = new CD("Songs of Innocence");
+    CD.clear();
+    assertEquals(CD.all().size(), 0);
+  }
+
+  @Test
+  public void getId_tasksInstantiateWithAnID_1() {
+    CD.clear();  // THIS TEST WILL FAIL WITHOUT THIS LINE!
+    CD myCD = new CD("Songs of Innocence");
+    assertEquals(1, myCD.getId());
+  }
+
+  @Test
+  public void find_returnsCD_WithSameId_secondCD() {
+    CD firstName = new CD("Songs of Innocence");
+    CD secondName = new CD("No Line on the Horizon");
+    assertEquals(CD.find(secondName.getId()), secondName);
+  }
+
+  @Test
+  public void find_returnsNullWhenNo_CD_Found_null() {
+    assertTrue(CD.find(999) == null);
+  }
 }
